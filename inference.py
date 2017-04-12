5fa2dc9c30b003e61106361a8cbc6720dcd32933
noda <noda@haw.co.jp>
2017-04-12 20:30:47 +0900

    :new: Make inference.py detect feces and inference

--- inference.py
+++ inference.py
@@ -10,22 +10,59 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 flags = tf.app.flags
 FLAGS = flags.FLAGS
 flags.DEFINE_integer('image_size', 48, 'Image size.')
+flags.DEFINE_string('cascade_path',
+                    './tmp/lbpcascade_animeface.xml', 'Path to cascade file.')
 
 TRAIN_DIR = './data/train'
 LOG_DIR = './log'
 dirs = os.listdir(TRAIN_DIR)
-CLASSES = [d for d in os.listdir(TRAIN_DIR) if os.path.isdir(os.path.join(TRAIN_DIR, d))]
+CLASSES = [d for d in os.listdir(TRAIN_DIR)
+           if os.path.isdir(os.path.join(TRAIN_DIR, d))]
 PIXEL_COUNT = FLAGS.image_size * FLAGS.image_size * 3
+CV_AA = 16
 
 
-test_images = []
+def detect(image):
+  if not os.path.isfile(FLAGS.cascade_path):
+    raise RuntimeError('%s not found' % FLAGS.cascade_path)
 
-for i in range(1, len(sys.argv)):
-  image = cv2.imread(sys.argv[i])
-  image = cv2.resize(image, (FLAGS.image_size, FLAGS.image_size))
-  test_images.append(image.flatten().astype(np.float32) / 255.0)
+  image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+  image_gray = cv2.equalizeHist(image_gray)
+  cascade = cv2.CascadeClassifier(FLAGS.cascade_path)
+  face_rects = cascade.detectMultiScale(image_gray,
+                                        scaleFactor=1.1,
+                                        minNeighbors=3,
+                                        minSize=(28, 28))
 
-test_images = np.asarray(test_images)
+  return face_rects
+
+
+def get_face_images(image, face_rects):
+  face_images = []
+
+  for (x, y, w, h) in face_rects:
+    face_image = image[y:y + h, x:x + w]
+    face_image = cv2.resize(face_image, (FLAGS.image_size, FLAGS.image_size))
+    face_image = face_image.flatten().astype(np.float32) / 255.0
+    face_images.append(face_image)
+
+  face_images = np.asarray(face_images)
+
+  return face_images
+
+
+def draw_prediction(image, face_rect, prediction):
+  x, y, w, h = face_rect
+
+  cv2.rectangle(image, (x, y), (x + w, y + h), (255, 255, 204), 2)
+  cv2.putText(image, CLASSES[prediction], (x, y),
+              cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 102), 1, CV_AA)
+
+  return image
+
+test_image = cv2.imread(sys.argv[1])
+face_rects = detect(test_image)
+face_images = get_face_images(test_image, face_rects)
 
 x = tf.placeholder(tf.float32, [None, PIXEL_COUNT])
 labels = tf.placeholder(tf.float32, [None, len(CLASSES)])
@@ -38,9 +75,17 @@ saver = tf.train.Saver()
 sess.run(tf.global_variables_initializer())
 saver.restore(sess, os.path.join(LOG_DIR, 'model.ckpt'))
 
-for i in range(len(test_images)):
-  softmax = sess.run(y, feed_dict={x: [test_images[i]], keep_prob: 1.0}).flatten()
+out_image = test_image.copy()
+
+for i in range(len(face_images)):
+  face_image = face_images[i]
+  softmax = sess.run(y, feed_dict={x: [face_image], keep_prob: 1.0}).flatten()
   prediction = np.argmax(softmax)
 
-  print(softmax)
-  print(CLASSES[prediction])
+  face_rect = face_rects[i]
+
+  draw_prediction(out_image, face_rect, prediction)
+
+  print(CLASSES[prediction], [n * 100 for n in softmax])
+
+cv2.imwrite('tmp/out.png', out_image)
